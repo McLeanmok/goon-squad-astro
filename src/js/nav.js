6b93378793ebf7424188c228d993ea9014d11b05
McLeanmok <165439265+McLeanmok@users.noreply.github.com>
2024-04-19 11:18:10 -0700

    Home Page Built

--- src/js/nav.js
+++
@@ -1,7 +0,0 @@
-const navLinks = document.querySelector("a");
-
-navLinks.array.forEach(element => {
-    if(navLinks.getAttribute("href") === window.location.pathname) {
-        navLinks.setAttribute("aria current", "page")
-    }
-});
