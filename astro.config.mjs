ac7ca3b1d7c2419208f2ff7de1e12525a208de44
McLeanmok <165439265+McLeanmok@users.noreply.github.com>
2024-04-27 13:37:55 -0700

    Updated URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://goon-squad.netlify.app",
+  site: "https://goon-squad-official.netlify.app/",
   integrations: [sitemap()]
 });
